0d7c36116c80621702445f5f98bbdd4105164b99
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2017-11-01 00:28:07 -0300

    Add solution for exercise 2 and add folder for exercise 6

--- exercise2/addDivisors_3and5_test.go
+++ exercise2/addDivisors_3and5_test.go
@@ -1,4 +1,4 @@
-package exercise1
+package main
 
 // Test, Benchmark and Example
 import "testing"
--- exercise2/exercise2.go
+++ exercise2/exercise2.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
 
 /*
 Exercise that adds all divisors of 3 and 5 of 0 to until value.
+Until value is received per parameter.
 */
 func main() {
 
 	input, _ := strconv.Atoi(os.Args[1])
 
-	addDivisors_3and5(input)
+	fmt.Println(addDivisors_3and5(input))
 
 }
 
 func addDivisors_3and5(until int) int {
 
 	var add int
-
 	for i := 0; i <= until; i++ {
 
 		if i%3 == 0 {
@@ -30,5 +31,6 @@ func addDivisors_3and5(until int) int {
 		}
 
 	}
+
 	return add
 }
